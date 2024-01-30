b8349cfdf0d84177a24b106e34a61eac31120bd9
Satakratu Chakraborty <satakratu.madhu@gmail.com>
2024-01-30 10:15:00 +0530

    hello

---
+++ script.js
@@ -0,0 +1,9 @@
+// let x=prompt("enter any number");
+// if(x<10)
+//     console.log("Hi,How are you");
+// else
+//     console.log("Hello world");
+console.log(5 & 1);
+console.log(7 | 1);
+console.log( 5 ^ 1);
+
