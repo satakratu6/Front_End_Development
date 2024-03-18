e66d504ac25b40819c4deb864f8f0fd323e491b1
satakratu6 <satakratu.madhu@gmail.com>
2024-03-18 10:50:53 +0530

    new

--- script3.js
+++ script3.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $.getJSON('data2.JSON',function(data){
         $.each(data,function(index, user){
-            $('#user-table tbody').append('<tr><td>'+user.reg_no+'</td><td>'+user.name+'</td><td>'+user.phone+'</td><td>'+user.dep+'</td><td>'+user.ADD+'</td></tr>');
+            $('#user-table tbody').append('<tr><td>'+user.reg_no+'</td><td>'+user.name+'</td><td>'+user.email+'</td><td>'+user.phone+'</td><td>'+user.ADD+'</td></tr>');
 
         });
     });
